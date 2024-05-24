5827cceffdd00abf99a8c599ec9163f1cfd733bc
edubruma <carlosvieiramadeira@gmail.com>
2024-05-24 20:23:32 -0300

    v.2

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ Criar um programa onde o usuário cria uma lista de tarefas a ser executado no d
 Ao terminar, crie um repositório local e suba o repositório remoto no github
 
 '''
+# importa biblioteca
+import os
+
 # lista vazia
 tarefas = []
 
 # laço de repetição
 while True:
     # usuário informa item da tarefa
-    nova_tarefa = input('Informe a nova tarefa ou Enter para encerrar e exibir a lista: ')
+    nova_tarefa = input('Insira a nova tarefa ou Enter para encerrar e exibir a lista: ')
 
     # verifica se o usuário inseriu nova tarefa
     if nova_tarefa != '':
@@ -18,11 +21,14 @@ while True:
     else:
         break
 
-# exibir a lista de tarefas
+# limpa console
+os.system('cls')
+
+# exibe a lista de taferas
+print(f'{'-'*30} LISTA DE TAREFAS {'-'*30}\n')
+
 for tarefa in tarefas:
     print(tarefa)
-
-
 
 
 
